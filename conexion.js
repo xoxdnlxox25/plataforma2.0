eac989434045d97deeb902194fb9d50b3cd8b9c2
xoxdnlxox25 <dnl25fl@gmail.com>
2025-05-13 12:55:15 -0400

    Update conexion.js

--- conexion.js
+++ conexion.js
@@ -1,2 +1,2 @@
 // URL de la API de Google Apps Script
-const URL = 'https://script.google.com/macros/s/AKfycbzUmFMcA_aE2vFoQvSEN3tNsjZ7f1CmXcobNT5fEnEKtGgAjm_FizmPBKlZHJ06ZFZ4/exec';
+const URL = 'https://script.google.com/macros/s/AKfycbxOzYXtEmAiVJ35X7O5y1SVORcaeZUjSaLFgmHiMHlhFIlZlvDzB23L6TnBETjzAIU/exec';
